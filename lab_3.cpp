567da5db11d1188c396eb9978a5ff2315de1a713
Владислав <andreysoloviov-1337@mail.ru>
2025-04-12 12:11:43 +1000

    Add files via upload

---
+++ lab_3.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char s[80];
+
+    printf("vvedite text: ");
+    fgets(s, 80, stdin);
+
+    for (int i = 0; i < strlen(s); i++)
+    {
+        if (s[i] == 'a') s[i] = 'A';
+        else if (s[i] == 'b') s[i] = 'B';
+        else if (s[i] == '!') s[i] = '?';
+    }
+    
+    printf("%s", s);
+    return 0;
+}
+
