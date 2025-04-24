2fce1bfaa908a27aa017c7491b54affc32bb4651
Владислав <andreysoloviov-1337@mail.ru>
2025-04-24 23:11:29 +1000

    Add files via upload

---
+++ lab_5.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "triangle.h"
+
+int main() {
+    double a, b, c;
+
+    printf("Vvedite dlini storon treygolnika (a b c): ");
+    scanf("%lf %lf %lf", &a, &b, &c);
+
+    if (!is_valid_triangle(a, b, c)) {
+        printf("Oshibka: iz takih storon nelzya postroit treygolnik.\n");
+        return 1;
+    }
+
+    double perimeter = calculate_perimeter(a, b, c);
+    double area = calculate_area(a, b, c);
+
+    printf("P treygolnika: %.2lf\n", perimeter);
+    printf("S treygolnika: %.2lf\n", area);
+
+    return 0;
+}
