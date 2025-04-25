e82524ac03118c5117e42a25b2c88a4bdf5a9884
Владислав <andreysoloviov-1337@mail.ru>
2025-04-26 09:52:06 +1000

    Add files via upload

---
+++ lab_6/lab_6.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    FILE *in = fopen("input.txt", "r");
+	FILE *out = fopen("output.txt", "w");
+    int year;
+    char name[100];
+    
+    while (fscanf(in, "%[^0-9]%d", name, &year) == 2)
+        if (year > 1980)
+            fprintf(out, "%s%d\n", name, year);
+    
+    fclose(in); fclose(out);
+    return 0;
+}
