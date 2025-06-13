1b38331f2dcf462171664a9ffec76c59fd3db391
Владислав <andreysoloviov-1337@mail.ru>
2025-06-13 22:58:11 +1000

    Add files via upload

---
+++ laba_11/lab_11.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        printf("Vvedite chisla: %s chislo1 chislo2 ...\n", argv[0]);
+        return 1;
+    }
+
+    double sum = 0, min = atof(argv[1]), max = atof(argv[1]);
+    int count = argc - 1;
+
+    for (int i = 1; i < argc; i++) {
+        double num = atof(argv[i]);
+        sum += num;
+        if (num < min) min = num;
+        if (num > max) max = num;
+    }
+
+    printf("Summa: %.2f\n", sum);
+    printf("Min: %.2f\n", min);
+    printf("Max: %.2f\n", max);
+    printf("Srednee: %.2f\n", sum / count);
+
+    return 0;
+}
